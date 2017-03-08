e8ce629e18586bc1cef926f1b4b2dd8e63832e03
Кондрашин Павел <wolfyw@live.ru>
2017-03-09 00:40:03 +0300

    Произошло изменение версии WindowsPhone с 8.1 на 10

---
+++ HubApp/HubApp/src/SQLiteConnection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubApp.src
+{
+    class SQLiteConnection: IRepository<PersonRank>
+    {
+
+
+
+
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
