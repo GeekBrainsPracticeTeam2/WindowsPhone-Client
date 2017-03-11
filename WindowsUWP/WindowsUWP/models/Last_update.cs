423aa005880a03602611493fa89a6ff9d16adcc9
Кондрашин Павел <wolfyw@live.ru>
2017-03-11 22:02:43 +0300

    Добавил пакеты для работы с SQLite. Добавил классы модели для локальной БД

---
+++ WindowsUWP/WindowsUWP/models/Last_update.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsUWP.models
+{
+    class Last_update
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public string table_name { get; set; }
+    }
+}
